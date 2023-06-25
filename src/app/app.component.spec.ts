40b1b241f5b28e9e6def3db8474ef1d5c4a68fe0
ViRGiL175 <virgil7g@gmail.com>
2023-06-25 16:39:50 +0400

    First submodule draft, SCSS migtation

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'spring-module-page'`, () => {
+  it(`should have as title 'pages'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('spring-module-page');
+    expect(app.title).toEqual('pages');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('spring-module-page app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('pages app is running!');
   });
 });
